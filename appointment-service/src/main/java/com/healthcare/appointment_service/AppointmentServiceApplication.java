a8a19e94f15c8b1f57cb10ee1cdb5bed32046f27
Kaushigithub <kypancholi2@gmail.com>
2025-01-03 13:59:50 +0530

    cors config added

--- appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
@@ -1,8 +1,14 @@
 package com.healthcare.appointment_service;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableFeignClients // Enable Feign client
@@ -25,7 +31,7 @@ public class AppointmentServiceApplication {
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
+		return new CorsFilter();
 	}
 
 }
